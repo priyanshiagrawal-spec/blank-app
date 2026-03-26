c47b9d8c2e1a3d3d866fff10fc5b666b95ccb259
priyanshiagrawal-spec <priyanshi.agrawal@trackwalkins.com>
2026-03-26 14:16:19 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("My First Real App 🚀")
+
+name = st.text_input("Enter your name")
+
+if name:
+    st.success(f"Hello {name} 👋")
